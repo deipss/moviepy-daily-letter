3730d1dccbfc7fc40e50128fdb1e9b3c0cc34d4a
deipss <deipss666@gmail.com>
2025-06-10 16:53:34 +0800

    generator

--- video_generator.py
+++ video_generator.py
@@ -10,6 +10,7 @@ from moviepy.video.fx import Loop
 from ollama_client import OllamaClient
 from logging_config import logger
 import sys
+from edge_tts import Communicate
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,7 +22,7 @@ INNER_WIDTH = GLOBAL_WIDTH - GAP
 INNER_HEIGHT = GLOBAL_HEIGHT - GAP
 W_H_RADIO = "{:.2f}".format(GLOBAL_WIDTH / GLOBAL_HEIGHT)
 FPS = 40
-MAIN_COLOR = "#FF9900"
+MAIN_COLOR = "##0099CC"
 MAIN_BG_COLOR = "#FFFFFF"
 VIDEO_FILE_NAME = "video.mp4"
 MATERIAL_PATH = 'material'
@@ -31,8 +32,16 @@ import time
 REWRITE = False
 
 
-def build_video_path(idx: str):
+def build_video_final_path(idx: str):
     return os.path.join(MATERIAL_PATH, idx, VIDEO_FILE_NAME)
+
+
+def build_video_path(idx: str, name: str):
+    return os.path.join(MATERIAL_PATH, idx, 'video_' + name + '.mp4')
+
+
+def build_audio_path(idx: str, name: str):
+    return os.path.join(MATERIAL_PATH, idx, 'audio_' + name + '.mp4')
 
 
 def build_video_img_path(idx: str, img_name: str):
@@ -160,60 +169,40 @@ def calculate_segment_times(duration, num_segments):
     return segment_times
 
 
-def generate_three_layout_video(audio_path, image_list, title, summary, output_path, index, is_preview=False):
-    title = "" + index + " " + title
+def generate_three_layout_video(audio_txt, image_list: list[dict], title, idx: str,
+                                is_preview=False):
+    # 计算各区域尺寸
+    title_height = INNER_HEIGHT * 0.15
+    top_height_ratio = 0.85
+    top_height = int((INNER_HEIGHT - title_height) * top_height_ratio)
+    bottom_height = INNER_HEIGHT - top_height - title_height
+    logger.info(f"title_height={title_height} - top_height={top_height} - bottom_height={bottom_height} ")
+
     # 加载背景和音频
     bg_clip = ColorClip(size=(INNER_WIDTH, INNER_HEIGHT), color=(255, 255, 255))  # 白色背景
-    audio_clip = AudioFileClip(audio_path)
+    communicate = Communicate(text=audio_txt, voice="zh-CN-YunyangNeural")
+    audio_temp_path = build_audio_path(idx, title)
+    communicate.save(audio_temp_path)
+    audio_clip = AudioFileClip(audio_temp_path)
     duration = audio_clip.duration
     bg_clip = bg_clip.with_duration(duration).with_audio(audio_clip)
-    bg_width, bg_height = bg_clip.size
 
-    # 计算各区域尺寸
-    title_height = 40
-    HEIGHT_RATIO = 0.75
-    top_height = int((bg_height - title_height) * HEIGHT_RATIO)
-    bottom_height = bg_height - top_height - title_height
+    # 图片处理
 
-    bottom_right_width = int(bg_width * 0.2)
-    bottom_left_width = bg_width - bottom_right_width
+    all_img_len = len(image_list)
 
-    # 右下图片处理 地球仪
-    bottom_right_img = VideoFileClip('videos/lady_announcer.mp4').with_effects([Loop(duration=duration)])
-    if bottom_right_img.w > bottom_right_width or bottom_right_img.h > bottom_height:
-        scale = min(bottom_right_width / bottom_right_img.w, bottom_height / bottom_right_img.h)
-        bottom_right_img = bottom_right_img.resized(scale)
-    bottom_right_img = bottom_right_img.with_position(('right', 'bottom')).with_duration(duration)
+    if all_img_len == 1:
+        first_img = image_list[0]
 
-    # 左上图片处理
-    segment_times = calculate_segment_times(duration, len(image_list))
-    image_clip_list = []
-    for image_path_top, (s, e) in zip(image_list, segment_times):
-        top_left_img = ImageClip(image_path_top)
-        scale = min(bg_width / top_left_img.w, top_height / top_left_img.h)
-        top_left_img = top_left_img.resized(scale)
-        offset_w, offest_h = (bg_width - top_left_img.w) // 2, (top_height - top_left_img.h) // 2
-        top_left_img = top_left_img.with_position((offset_w, offest_h + title_height)).with_start(s).with_duration(
-            e - s)
-        image_clip_list.append(top_left_img)
 
-    # 左下文字处理
-    font_size, chars_per_line = calculate_font_size_and_line_length(summary, bottom_left_width * 95 / 100,
-                                                                    bottom_height * 95 / 100)
-    summary = '\n'.join([summary[i:i + chars_per_line] for i in range(0, len(summary), chars_per_line)])
-    bottom_left_txt = TextClip(
-        text=summary,
-        interline=font_size // 2,
-        font_size=font_size,
-        color='black',
-        font='./font/simhei.ttf',
-        text_align='center',
-        size=(bottom_left_width, bottom_height),
-        method='caption'
-    ).with_duration(duration).with_position(('left', top_height + title_height))
+
+
+
+
+
 
     # 标题
-    title_font_size = 40
+    title_font_size = int(title_height * 0.9)
     top_title = TextClip(
         interline=title_font_size // 2,
         text=title,
@@ -223,18 +212,16 @@ def generate_three_layout_video(audio_path, image_list, title, summary, output_p
         method='label'
     ).with_duration(duration).with_position(('left', 'top'))
 
-    # 创建各区域背景框
-
     # 合成最终视频
+    image_clip_list = []
     image_clip_list.insert(0, bg_clip)
-    image_clip_list.insert(1, bottom_left_txt)
-    image_clip_list.insert(2, bottom_right_img)
     image_clip_list.insert(3, top_title)
-    final_video = CompositeVideoClip(clips=image_clip_list, size=(bg_width, bg_height))
+    final_video = CompositeVideoClip(clips=image_clip_list, size=(INNER_WIDTH, INNER_HEIGHT))
     if is_preview:
         final_video.preview()
     else:
-        final_video.write_videofile(output_path, codec="libx264", audio_codec="aac", fps=FPS)
+        final_video.write_videofile(build_video_path(idx, title), remove_temp=True, codec="libx264", audio_codec="aac",
+                                    fps=FPS)
 
 
 def get_full_date(today=datetime.now()):
@@ -271,225 +258,6 @@ def get_weekday_color():
     return weekday_color_map[weekday]
 
 
-def generate_video_introduction(output_path='temp/introduction.mp4', today=datetime.now().strftime("%Y%m%d"),
-                                is_preview=False):
-    """生成带日期文字和背景音乐的片头视频
-
-    Args:
-        bg_music_path: 背景音乐文件路径
-        output_path: 输出视频路径
-    """
-    generate_background_image(GLOBAL_WIDTH, GLOBAL_HEIGHT)
-    if os.path.exists(output_path) and not REWRITE:
-        logger.info(f"片头{output_path}已存在,直接返回")
-        # todo
-        return "", 0
-        # 加载背景图片
-    bg_clip = ImageClip(BACKGROUND_IMAGE_PATH)
-
-    # 加载背景音乐
-    date_obj = datetime.strptime(today, "%Y%m%d")
-    date_text = get_full_date(date_obj)
-    audio_clip = AudioFileClip(audio_path)
-    duration = audio_clip.duration
-
-    # 设置背景视频时长
-    bg_clip = bg_clip.with_duration(duration).with_audio(audio_clip)
-
-    # 创建日期文字
-
-    date_parts = date_text.split('\n')
-    max_length = max(len(part) for part in date_parts) if date_parts else len(date_text)
-
-    txt_clip = TextClip(
-        text=date_text,
-        font_size=int(GLOBAL_WIDTH / max_length * 0.8),
-        color=MAIN_COLOR,
-        font='./font/simhei.ttf',
-        stroke_color='black',
-        stroke_width=2
-    ).with_duration(duration).with_position(('center', 0.7), relative=True)
-
-    topics = generate_top_topic_by_ollama(today)
-    logger.info(f"topics={topics}")
-    topic_txt_clip = TextClip(
-        text=topics,
-        font_size=int(GLOBAL_HEIGHT * 0.75 / 5 * 0.6),
-        color=get_weekday_color(),
-        interline=int(GLOBAL_HEIGHT * 0.75 / 5 * 0.6) // 4,
-        font='./font/simhei.ttf',
-        stroke_color=MAIN_COLOR,
-        stroke_width=3
-    ).with_duration(duration).with_position(('center', 0.1), relative=True)
-
-    # 合成最终视频
-    final_clip = CompositeVideoClip([bg_clip, txt_clip, topic_txt_clip], size=bg_clip.size)
-    if is_preview:
-        final_clip.preview()
-    else:
-        final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac", fps=FPS)
-    return topics, duration
-
-
-def combine_videos_with_transitions(video_paths, output_path, topics, today):
-    bg_clip = ImageClip(BACKGROUND_IMAGE_PATH)
-
-    # 加载视频和音频
-    clips = []
-    for i, video_path in enumerate(video_paths):
-        # 加载视频
-        video = VideoFileClip(video_path)
-        if (video.duration < 2):
-            continue
-        video = video.with_position(('center', 'center'), relative=True)
-        # 将视频放置在背景上
-        video_with_bg = CompositeVideoClip([
-            bg_clip,
-            video
-        ], use_bgclip=True)
-        # 将视频放置在背景上
-        clips.append(video_with_bg)
-
-    final_clip = concatenate_videoclips(clips, method="compose")
-    # 导出最终视频
-    final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac", fps=FPS)
-    save_today_news_json(topics, today)
-    logger.info(f"视频整合生成完成,path={output_path}")
-    # final_clip.preview()
-
-
-def combine_videos(today: str = datetime.now().strftime("%Y%m%d")):
-    start_time = time.time()
-    video_paths = []
-    logger.info(f"正在生成视频{intro_path}...")
-    topics, duration = generate_video_introduction(intro_path, today)
-    video_paths.append(intro_path)
-    for i in range(max(len(bbc_paths), len(cn_paths))):
-        if i < len(cn_paths):
-            video_paths.append(cn_paths[i])
-        if i < len(bbc_paths):
-            video_paths.append(bbc_paths[i])
-    combine_videos_with_transitions(video_paths, build_today_final_video_path(today), topics, today)
-    end_time = time.time()  # 结束计时
-    elapsed_time = end_time - start_time
-    logger.info(f"视频整合生成总耗时: {elapsed_time:.2f} 秒")
-
-
-def generate_all_news_video(source: str, today: str = datetime.now().strftime("%Y%m%d")) -> list[str]:
-    folder_path = os.path.join(CN_NEWS_FOLDER_NAME, today, source)
-    json_file_path = os.path.join(folder_path, PROCESSED_NEWS_JSON_FILE_NAME)
-
-    with open(json_file_path, 'r', encoding='utf-8') as json_file:
-        news_data = json.load(json_file)
-    video_output_paths = []
-    for news_item in news_data:
-        article = NewsArticle(**news_item)
-        if not article.show:
-            logger.info(f"{article.source}{article.folder}{article.title}新闻已隐藏，跳过生成")
-            continue
-
-        # 新增逻辑：将摘要转换为音频并保存
-        folder_path = os.path.dirname(json_file_path)  # 获取新闻图片所在的文件夹路径
-        video_output_path = os.path.join(folder_path, article.folder, "%s" % VIDEO_FILE_NAME)
-        if os.path.exists(video_output_path) and not REWRITE:
-            logger.info(f"{article.source}{article.folder}视频已存在，跳过生成,path={video_output_path}")
-            video_output_paths.append(video_output_path)
-            continue
-        logger.info(f"{article.folder}{article.title}新闻开始生成")
-
-        audio_output_path = os.path.join(folder_path, article.folder, "%s" % AUDIO_FILE_NAME)
-        img_list = []
-        for image in article.images:
-            img_list.append(os.path.join(folder_path, article.folder, image))
-        generate_three_layout_video(
-            output_path=video_output_path,
-            audio_path=audio_output_path,
-            image_list=img_list,
-            summary=article.summary,
-            title=article.title,
-            index=article.folder,
-            is_preview=False
-        )
-        video_output_paths.append(video_output_path)
-    return video_output_paths
-
-
-def load_json_by_source(source, today):
-    folder_path = os.path.join(CN_NEWS_FOLDER_NAME, today, source)
-    json_file_path = os.path.join(folder_path, PROCESSED_NEWS_JSON_FILE_NAME)
-    with open(json_file_path, 'r', encoding='utf-8') as json_file:
-        news_data = json.load(json_file)
-    return json_file_path, news_data
-
-
-def save_today_news_json(topic, today: str = datetime.now().strftime("%Y%m%d")):
-    _, cn = load_json_by_source(CHINADAILY, today)
-    _, bbc = load_json_by_source(BBC, today)
-    urls = []
-    [urls.append(i['url']) for i in cn]
-    [urls.append(i['url']) for i in bbc]
-
-    json_file_path = build_today_json_path(today)
-    if os.path.exists(json_file_path):
-        json_data = json.load(open(json_file_path, 'r', encoding='utf-8'))
-        json_data['topic'] += topic.repalce("\n", "|")
-        json_data['urls'].append(urls)
-    else:
-        json_data = {
-            'topic': topic.repalce("\n", "|"),
-            'urls': urls
-        }
-    json.dump(json_data, open(json_file_path, 'w', encoding='utf-8'), ensure_ascii=False, indent=4)
-    logger.info(f"保存今日新闻json文件到 {json_file_path}")
-    logger.info(f"今日新闻json文件\n {json_data}")
-
-
-def generate_top_topic_by_ollama(today: str = datetime.now().strftime("%Y%m%d")) -> str:
-    client = OllamaClient()
-    folder_path = os.path.join(CN_NEWS_FOLDER_NAME, today, BBC)
-    json_file_path = os.path.join(folder_path, PROCESSED_NEWS_JSON_FILE_NAME)
-
-    with open(json_file_path, 'r', encoding='utf-8') as json_file:
-        news_data = json.load(json_file)
-    txt = ";".join([news_item['title'] for news_item in news_data])
-    data = client.generate_top_topic(txt)
-    logger.info(f'topic is \n{data}')
-    return data
-
-
-def test_generate_all():
-    today = '20250604'
-    generate_all_news_video(source=BBC, today=today)
-    generate_all_news_video(source=CHINADAILY, today=today)
-    generate_background_image(GLOBAL_WIDTH, GLOBAL_HEIGHT)
-    generate_video_introduction()
-    combine_videos_with_transitions(
-        ['cn_news/20250512/intro.mp4', 'cn_news/20250512/0000/video.mp4', 'cn_news/20250512/0001/video.mp4'], 'a.mp4',
-        '', today)
-
-
-def test_generate_video_introduction():
-    REWRITE = True
-    generate_video_introduction(today='20250606', is_preview=True)
-
-
-def test_video_text_align():
-    list = [
-        'news/20250604/chinadaily/0000/683fd3d86b8efd9fa6284ef8_m.png',
-        'news/20250604/chinadaily/0000/683fd3d96b8efd9fa6284efa_m.jpg',
-        'news/20250604/chinadaily/0000/683fd3d96b8efd9fa6284efc_m.jpg',
-        'news/20250604/chinadaily/0000/683fd3da6b8efd9fa6284efe_m.jpg'
-    ]
-
-    generate_three_layout_video(
-        output_path="news/20250604/chinadaily/0000/video.mp4",
-        audio_path="news/20250604/chinadaily/0000/summary_audio.aiff",
-        image_list=list,
-        summary="""韩国新总统李在镕以近50%的选票胜出，但其蜜月期仅一天即上任，需应对弹劾前总统尹锡烈留下的政治和安全漏洞。首轮挑战是处理唐纳德·特朗普可能破坏的经济、安全和与朝鲜关系。一季度韩国经济收缩，已因特朗普征收25%关税陷入困境。美国驻首尔军事存在可能转向遏制中国，增加韩国的外交和军事压力。李明博希望改善与中国的关系，但面临美国对朝鲜半岛战略布局的不确定性，同时需解决国内民主恢复问题。""",
-        title="""[英国广播公司]韩国新总统需要避免特朗普式的危机""",
-        index="0000",
-        is_preview=False
-    )
 
 
 import argparse
@@ -517,4 +285,4 @@ if __name__ == "__main__":
         REWRITE = True
         logger.info("指定强制重写")
 
-    combine_videos(today=args.today)
+    # combine_videos(today=args.today)
